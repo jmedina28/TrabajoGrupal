97015bee8b053ef8d27c44682c0ca235ed22d6a5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-15 17:34:38 +0100

    probando

--- ejercicio1.py
+++ ejercicio1.py
@@ -12,13 +12,8 @@ def suma_matriz(matriz):
 
 if __name__ == "__main__":
     fptr = open(os.environ["OUTPUT_PATH"] + "solucion1.txt", "w")
-
     contar_matriz = int(input())
-
     matriz = list(map(int, input().rstrip().split()))
-
     result = suma_matriz(matriz)
-
     fptr.write(str(result) + "\n")
-
     fptr.close()
