d8555f8c279de1dd481ee884a3555936e64ab97e
Juan Medina <juanperezmedina67@gmail.com>
2021-12-15 18:07:23 +0100

    dejo creada la parte en la que se desarrolla el fichero de texto y se presenta la solución

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,6 +1,7 @@
 # Importamos las librerías.
 import os
 
+os.environ["Salida"] = ""
 # Defino la función que va a resolver el ejercicio.
 def sumagrandematriz(matriz):
     # Defino la variable en la que quedará registrado el resultado de la operación.
@@ -9,3 +10,17 @@ def sumagrandematriz(matriz):
     for n in matriz:
         sumamatrices += n
     return sumamatrices
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["Salida"] + "solucion3.txt", "w")
+    print("Escriba un número:\n ")
+    ar_count = int(input().strip())
+    print("Escriba los números de la matriz separados por espacios:\n ")
+    matriz = list(map(int, input().rstrip().split()))
+
+    resultado = sumagrandematriz(matriz)
+
+    fptr.write(str(resultado) + "\n")
+
+    fptr.close()
