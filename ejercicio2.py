39af64c86882a7193730ca12c2cbba2dfdf32773
Juan Medina <juanperezmedina67@gmail.com>
2021-12-15 17:25:47 +0100

    defino las variables dentro de la función

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,8 @@
 # Importamos las librerías necesarias para la realización del ejercicio.
 import os
+
 # Creo la función en la que se va a desarrollar el ejercicio.
 def comparalosproblemas():
+    # Defino las variables con los nombres de los jugadores.
+    lucia = 0
+    carlos = 0
