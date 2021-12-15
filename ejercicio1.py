d999e00490e633746c750e75312f02c9177fd7a8
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-15 17:29:35 +0100

    creamos el fichero que nos dará la solución

--- ejercicio1.py
+++ ejercicio1.py
@@ -9,3 +9,16 @@ def suma_matriz(matriz):
     for i in range(len(matriz)):
         suma += matriz[i]
     return suma
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"] + "solucion1.txt", "w")
+
+    contar_matriz = int(input())
+
+    matriz = list(map(int, input().rstrip().split()))
+
+    result = suma_matriz(matriz)
+
+    fptr.write(str(result) + "\n")
+
+    fptr.close()
