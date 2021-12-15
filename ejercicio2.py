b544f4b1a2eb8e87958e8f336efe369c6b525403
Juan Medina <juanperezmedina67@gmail.com>
2021-12-15 17:30:01 +0100

    desarrollo el bucle y finalizo la función

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,7 +2,14 @@
 import os
 
 # Creo la función en la que se va a desarrollar el ejercicio.
-def comparalosproblemas():
+def comparalosproblemas(a, b):
     # Defino las variables con los nombres de los jugadores.
     lucia = 0
     carlos = 0
+    # Creo el bucle que va a completar los datos de las variables.
+    for n in range(3):
+        if a[n] > b[n]:
+            lucia += 1
+        elif a[n] < b[n]:
+            carlos += 1
+    return (lucia, carlos)
