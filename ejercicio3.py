92e6bf67324d83e6b2b5c12a913697f34c1c3473
Juan Medina <juanperezmedina67@gmail.com>
2021-12-15 18:16:09 +0100

    defino la variable sobre la que queda registrado el resultado

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,3 +3,5 @@ import os
 
 # Defino la función que va a resolver el ejercicio.
 def sumagrandematriz(matriz):
+    # Defino la variable en la que quedará registrado el resultado de la operación.
+    sumamatrices = 0
