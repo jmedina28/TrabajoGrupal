8b9bce8f2ad01b81e8c63e09ce5f506a0ff373e4
Juan Medina <juanperezmedina67@gmail.com>
2021-12-15 18:00:37 +0100

    finalizo el bucle y la función

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,6 +2,10 @@
 import os
 
 # Defino la función que va a resolver el ejercicio.
-def sumagrandematriz():
+def sumagrandematriz(matriz):
     # Defino la variable en la que quedará registrado el resultado de la operación.
     sumamatrices = 0
+    # Voy a crear el bucle para hacer la suma.
+    for n in matriz:
+        sumamatrices += n
+    return sumamatrices
