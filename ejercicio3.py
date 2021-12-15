56c3daad487ea74d4cd2e79d843289bb19ed72dd
Juan Medina <juanperezmedina67@gmail.com>
2021-12-15 18:18:24 +0100

    se crea el fichero sobre el que se registra el resultado y defino las variables inputeables

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,6 +1,7 @@
 # Importamos las librerías.
 import os
 
+os.environ["Salida"] = ""
 # Defino la función que va a resolver el ejercicio.
 def sumagrandematriz(matriz):
     # Defino la variable en la que quedará registrado el resultado de la operación.
@@ -9,3 +10,21 @@ def sumagrandematriz(matriz):
     for n in matriz:
         sumamatrices += n
     return sumamatrices
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["Salida"] + "solucion3.txt", "w")
+    ar_count = int(input("Escriba un número:\n").strip())
+    matriz = list(
+        map(
+            int,
+            input("Escriba los números de la matriz separados por espacios:\n")
+            .rstrip()
+            .split(),
+        )
+    )
+    resultado = sumagrandematriz(matriz)
+
+    fptr.write(str(resultado) + "\n")
+
+    fptr.close()
