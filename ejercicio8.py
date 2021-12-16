21cfeb8eedd205c48bafc64437d3dbbd3c622649
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-16 16:55:04 +0100

    el programa nos devuelve el numero de naranjas y manzanas que han entrado

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,5 +1,12 @@
 def contador(a, b, c, d, manzanas, naranjas):
     num_manzanas_dentro = 0
     num_naranjas_dentro = 0
-    from manzana1 in manzanas
+    for manzana1 in manzanas:
+        if c + manzana1 >= a and c <= b:
+            num_manzanas_dentro += 1
+    for naranja1 in naranjas:
+        if d + naranja1 >= a and d <= b:
+            num_naranjas_dentro += 1
     
+    print("Han entrado ", str(num_manzanas_dentro), " manzanas")
+    print("Han entrado ", str(num_naranjas_dentro), " naranjas")
