dcde4d75d076938a75cd2a8e505586299071bb01
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-16 17:28:14 +0100

    error

--- ejercicio8.py
+++ ejercicio8.py
@@ -31,8 +31,8 @@ d = int(segundo[1])
 e = int(tercero[0])
 f = int(tercero[1])
 
-manzanas = list(map(int(input("e nº separados por espacios (distancias a las que cada manzana cae desde el punto c)").rstrip().split())))
-naranjas = list(map(int(input("f nº separados por espacios (distancias a las que cada naranja cae desde el punto d)").rstrip().split())))
+manzanas = list(map(int(input(e, " nº separados por espacios (distancias a las que cada manzana cae desde el punto c)").rstrip().split())))
+naranjas = list(map(int(input(f," nº separados por espacios (distancias a las que cada naranja cae desde el punto d)").rstrip().split())))
 
 contador =(a, b, c, d, naranjas, manzanas)
 
