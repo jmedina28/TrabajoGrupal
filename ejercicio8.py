3c0cd26023dbb458b15db87171cdb65cdcb97883
Juan Medina <juanperezmedina67@gmail.com>
2021-12-18 10:45:27 +0100

    modificación final del ejercicio 8

--- ejercicio8.py
+++ ejercicio8.py
@@ -7,6 +7,7 @@
 # 2º nº: naranjas que caen
 import re
 
+
 def contador(a, b, c, d, manzanas, naranjas):
     num_manzanas_dentro = 0
     num_naranjas_dentro = 0
@@ -16,12 +17,25 @@ def contador(a, b, c, d, manzanas, naranjas):
     for naranja1 in naranjas:
         if d + naranja1 >= a and d <= b:
             num_naranjas_dentro += 1
-    
+
     print("Han entrado ", str(num_manzanas_dentro), " manzanas")
     print("Han entrado ", str(num_naranjas_dentro), " naranjas")
 
-primer = input("Introduzca dos números separados por espacios que serán la amplitud de la casa: ").rstrip().split()
-segundo = input("Introduzca dos números separados por espacios que serán la distancia entre los dos árboles: )").rstrip().split()
+
+primer = (
+    input(
+        "Introduzca dos números separados por espacios que serán la amplitud de la casa: "
+    )
+    .rstrip()
+    .split()
+)
+segundo = (
+    input(
+        "Introduzca dos números separados por espacios que serán la distancia entre los dos árboles: "
+    )
+    .rstrip()
+    .split()
+)
 tercero = input("Introduzca dos nº separados por espacios: ").rstrip().split()
 
 a = int(primer[0])
@@ -31,8 +45,13 @@ d = int(segundo[1])
 e = int(tercero[0])
 f = int(tercero[1])
 
-manzanas = list(map(int(input(e, " nº separados por espacios (distancias a las que cada manzana cae desde el punto c)").rstrip().split())))
-naranjas = list(map(int(input(f," nº separados por espacios (distancias a las que cada naranja cae desde el punto d)").rstrip().split())))
+print(
+    " nº separados por espacios (distancias a las que cada manzana cae desde el punto c)",
+)
+manzanas = list(map(int, input().rstrip().split()))
+print(
+    " nº separados por espacios (distancias a las que cada naranja cae desde el punto d)",
+)
+naranjas = list(map(int, input().rstrip().split()))
 
-contador =(a, b, c, d, naranjas, manzanas)
-
+contador(a, b, c, d, naranjas, manzanas)
