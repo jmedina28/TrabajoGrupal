41a3cf9fb485029dd306bdddbc086d162006cd42
Juan Medina <juanperezmedina67@gmail.com>
2021-12-18 14:20:36 +0100

    modificaciones finales

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,37 +1,43 @@
 import os
 
-os.environ["OUTPUT_PATH"] = " "
+os.environ["OUTPUT_PATH"] = ""
 
 
-def juego_de_piedras(n): #n es el número inicial de piedras
-    ganador = " "
-    jugador1 = input("Introduzca el nombre del jugador 1: ")
-    jugador2 = input("Introduzca el nombre del jugador 2: ")
+def juego_de_piedras(n):  # n es el número inicial de piedras
+    ganador = ""
+
     if jugada(n) != 0:
-        ganador = "Ha ganado ", jugador1 #jugador 1 siempre juega primero
+        ganador = "Ha ganado el jugador 1."  # jugador 1 siempre juega primero
     else:
-        ganador = "Ha ganado ", jugador2
-        return ganador
+        ganador = "Ha ganado el jugador 2."
+    return ganador
+
 
 def jugada(n):
     bienjugado = 0
-    modelo = n % 7
-    if modelo >= 2 and modelo <= 3:
+    modulo = n % 7
+    if modulo >= 2 and modulo <= 3:
         bienjugado = 2
-    elif modelo == 4:
+    elif modulo == 4:
         bienjugado = 3
-    elif modelo >= 5 and modelo <= 6:
-        return bienjugado
+    elif modulo >= 5 and modulo <= 6:
+        bienjugado = 5
+    return bienjugado
 
-fptr = open(os.environ["OUTPUT_PATH"] + "solución5.txt", "w")
-print("Introduce el número de casos: ")
-t = int(input().strip()) #número de casos
 
-for t in range(t):
-    print("Introduzca ", t, " números enteros (serán las piedras en un caso de prueba)")
-    n = int(input().strip())
-    final = juego_de_piedras(n)
-    fptr.write(final + "\n")
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"] + "solución5.txt", "w")
+    print("Introduce el número de casos: ")
+    t = int(input().strip())  # número de casos
 
-fptr.close()
+    for ti in range(t):
+        print(
+            "Introduzca ",
+            t,
+            " números enteros (serán las piedras en un caso de prueba)",
+        )
+        n = int(input().strip())
+        final = juego_de_piedras(n)
+        fptr.write(str(final) + "\n")
 
+    fptr.close()
