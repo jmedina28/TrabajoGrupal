91993b4115f44543a514f5a9553ff556a20da05b
Juan Medina <juanperezmedina67@gmail.com>
2021-12-18 10:27:44 +0100

    dejo desarrollada la función de notafinal donde se llevan a cabo los redondeos.

--- ejercicio7.py
+++ ejercicio7.py
@@ -3,7 +3,7 @@ import os
 os.environ["OUTPUT_PATH"] = "solución7.txt"
 
 
-def notasEstudiantes():
+def notasEstudiantes(notas):
     lista = []
     for (
         i
@@ -14,3 +14,17 @@ def notasEstudiantes():
             notafinal(i)
         )  # notafinal va a ser la función que hace los redondeos
     return lista
+
+
+def notafinal(notas):
+    notaredondeada = 0
+    if notas < 40:
+        notas = notaredondeada
+    else:
+        cocientes = int(notas / 5 + 1)
+        multiplo = int(cocientes * 5)
+        if multiplo - notas < 3:
+            multiplo = notaredondeada
+        else:
+            notas = notaredondeada
+    return notaredondeada
