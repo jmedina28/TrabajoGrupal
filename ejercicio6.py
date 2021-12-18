bb8a63e845be4cac0704fc8b152b9db0944228b0
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-18 10:57:03 +0100

    definición de función comparación definida. V o F

--- ejercicio6.py
+++ ejercicio6.py
@@ -2,3 +2,9 @@ class Coordenadas:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def comparacion(self, x, y):
+        if self.x == x and self.y == y:
+            return True
+        else: 
+            return False
